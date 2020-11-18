fbb86140cc7a93ec3363781fe59c5d8570ae88fc
w127456 <74294701+w127456@users.noreply.github.com>
2020-11-18 09:14:04 +0800

    Create 111801.hh.c

---
+++ 111801.hh.c
@@ -0,0 +1,36 @@
+#include<stdio.h>
+void main(){
+	int a=3,b=4,c=5,x,y;
+	if(a+b>c&&b==c){
+		printf("为真命题\n");}
+	else {
+		printf("为假命题\n");
+	
+	}
+	if(a||b+c&&b-c){
+		printf("为真命题\n");}
+	else {
+		printf("为假命题\n");
+	
+	}
+	if(!(a>b)&&!c||1){
+		printf("为真命题\n");}
+	else {
+		printf("为假命题\n");
+	
+	}
+	if(!(x=a)&&(y=b)&&0){
+		printf("为真命题\n");}
+	else {
+		printf("为假命题\n");
+	
+	}
+	if(!(a+b)+c-1&&b+c/2){
+		printf("为真命题\n");}
+	else {
+		printf("为假命题\n");
+	
+	}
+
+
+}
