0cae14b2d01d75159511ea84f801e2e72c30d910
w127456 <74294701+w127456@users.noreply.github.com>
2020-11-25 09:45:22 +0800

    Create 1120502.QS大学排名.c

---
+++ 1120502.QS大学排名.c
@@ -0,0 +1,38 @@
+#include<stdio.h>
+void main(){
+int a;
+scanf("%d",&a);
+if(a==1){
+printf("第一大学");
+}
+else if(a==2){
+printf("第二大学");
+}
+else if(a==3){
+printf("第三大学");
+}
+else if(a==4){
+printf("第四大学");
+}
+else if(a==5){
+printf("第五大学");
+}
+else if(a==6){
+printf("第六大学");
+}
+else if(a==7){
+printf("第七大学");
+}
+else if(a==8){
+printf("第八大学");
+}
+else if(a==9){
+printf("第九大学");
+}
+else if(a==10){
+printf("第十大学");
+}
+else{
+printf("在座的各位都是垃圾");
+}
+}
