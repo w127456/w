a473862a4ae8e5fa41941f298aeac91323710f6d
w127456 <74294701+w127456@users.noreply.github.com>
2020-12-02 08:53:17 +0800

    Create 120201.switch语句.app.c

---
+++ 120201.switch语句.app.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+void main(){
+int n;
+printf("QS大学排名\n");
+scanf("%d",&n);
+switch(n){
+case 1:printf("第一大学");break;
+case 2:printf("第二大学");break;
+case 3:printf("第三大学");break;
+case 4:printf("第四大学");break;
+case 5:printf("第五大学");break;
+case 6:printf("第六大学");break;
+case 7:printf("第七大学");break;
+case 8:printf("第八大学");break;
+case 9:printf("第九大学");break;
+case 10:printf("第十大学");break;
+case 11:printf("第十一大学");break;
+case 12:printf("第十二大学");break;
+case 13:printf("第十三大学");break;
+case 14:printf("第十四大学");break;
+case 15:printf("第十五大学");break;
+case 16:printf("第十六大学");break;
+case 17:printf("第十七大学");break;
+case 18:printf("第十八大学");break;
+case 19:printf("第十九大学");break;
+case 20:printf("第二十大学");break;
+default:printf("错了");break;
+}
+}
