fba0d444031cc524474d0e778b776fd39ea5de36
w127456 <74294701+w127456@users.noreply.github.com>
2020-12-09 09:44:08 +0800

    Update 120902.数组元素递增.app.c

--- 120902.数组元素递增.app.c
+++ 120902.数组元素递增.app.c
@@ -3,12 +3,12 @@
 void main(){
 int i=0;
 int a,b;
-int c[9][10]={
+int c[9][20]={
 	{0,0,0,0},
 	{0,0,0,0}
 };
 for(a=0;a<9;a++){
-	for(b=0;b<10;b++){
+	for(b=0;b<20;b++){
 		i++;
 	c[a][b]=i;
 	printf("%d ",c[a][b]);
