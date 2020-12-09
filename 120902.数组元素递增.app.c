2c3181bd340d74e07c40523aeb4b9c313089e88e
w127456 <74294701+w127456@users.noreply.github.com>
2020-12-09 09:41:48 +0800

    Create 120902.数组元素递增.app.c

---
+++ 120902.数组元素递增.app.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<windows.h>
+void main(){
+int i=0;
+int a,b;
+int c[9][10]={
+	{0,0,0,0},
+	{0,0,0,0}
+};
+for(a=0;a<9;a++){
+	for(b=0;b<10;b++){
+		i++;
+	c[a][b]=i;
+	printf("%d ",c[a][b]);
+	}
+	printf("\n");
+}
+}
